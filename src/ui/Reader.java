8ada97e3866d449639f7d0098905cabdce0e5181
Andres Parra <andresdavidparra2002@gmail.com>
2023-03-05 12:17:25 -0500

    Método de generación de número aleatorio

--- src/ui/Reader.java
+++ src/ui/Reader.java
@@ -3,11 +3,11 @@ package ui;
 import java.util.Scanner;
 
 public class Reader {
-    
+
     private static Scanner reader = new Scanner(System.in);
 
-    public static int readInt(int def){
-        if(reader.hasNextInt()){
+    public static int readInt(int def) {
+        if (reader.hasNextInt()) {
             return reader.nextInt();
         }
         reader.next();
@@ -15,10 +15,10 @@ public class Reader {
         return def;
     }
 
-    public static int readInt(int def, int min){
-        if(reader.hasNextInt()){
+    public static int readInt(int def, int min) {
+        if (reader.hasNextInt()) {
             int value = reader.nextInt();
-            if(value >= min){
+            if (value >= min) {
                 return value;
             }
             System.out.println("Valor por debajo del mínimo. Valor tomado: " + min);
@@ -29,7 +29,18 @@ public class Reader {
         return def;
     }
 
-    public static String readString(){
+    public static String readString() {
         return reader.next();
+    }
+
+    public static int randInt(int start, int end) {
+        if(end > start){
+            return (int) ((Math.random() * (1 + end - start)) + start);
+        }
+        return start;
+    }
+
+    public static int randInt(int end) {
+        return (int) (Math.random() * (end+1));
     }
 }
