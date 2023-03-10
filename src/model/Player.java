075b6177fa2934be3c72377e7110fd231765bcd6
gerson05 <1115449382@u.icesi.edu.co>
2023-03-10 15:17:12 -0500

    avance arbol de puntajes

--- src/model/Player.java
+++ src/model/Player.java
@@ -1,9 +1,15 @@
 package model;
 
+import org.w3c.dom.Node;
+
 public class Player {
 
     private char symbol;
     private int position;
+    private Node right;
+    private Node left;
+    private int points;
+    private String name;
 
     private Player next;
     private Player previous;
@@ -43,6 +49,69 @@ public class Player {
 
     public void setPrevious(Player previous) {
         this.previous = previous;
+    }
+
+
+    /**
+     * @return Node return the right
+     */
+    public Node getRight() {
+        return right;
+    }
+
+    /**
+     * @param right the right to set
+     */
+    public void setRight(Node right) {
+        this.right = right;
+    }
+
+    /**
+     * @return Node return the left
+     */
+    public Node getLeft() {
+        return left;
+    }
+
+    /**
+     * @param left the left to set
+     */
+    public void setLeft(Node left) {
+        this.left = left;
+    }
+
+
+   
+
+
+
+    /**
+     * @return int return the points
+     */
+    public int getPoints() {
+        return points;
+    }
+
+    /**
+     * @param points the points to set
+     */
+    public void setPoints(int points) {
+        this.points = points;
+    }
+
+
+    /**
+     * @return String return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
     }
 
 }
